146848b584afea873569e91cd07361d8813f992e
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2022-10-19 17:35:14 +0200

    add FormTranslate component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
+import FormTranslate from './components/FormTranslate'
 
 function App() {
+  const [number, setNumber] = useState();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ekko test technique</h1>
       </header>
+      <section>
+        <h3>Entrer un nombre à traduire...</h3>
+        <form>
+          <label for="number">Nombre :</label>
+          <input type="number" name="number" min="0" max="9" value={number} onChange={e => setNumber(e.target.value)} />
+        </form>
+        <FormTranslate number={number} />
+      </section>
     </div>
   );
 }
