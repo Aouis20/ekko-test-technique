571d59dbcb1fd7d7e16cbae8831de814f5052898
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2022-10-20 16:21:53 +0200

    update FormTranslate to Translate Sentence & start UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React, { useState } from 'react';
-import FormTranslate from './components/FormTranslate'
+import TranslateSentence from './components/TranslateSentence'
+import './App.css'
 
 function App() {
-  const [number, setNumber] = useState();
-
+  const [number, setNumber] = useState(0);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Ekko test technique</h1>
+
+      {/* Header */}
+      <header>
+
+        {/* Society */}
+        <div className='society'>
+          <a href='https://www.ekko-formations.com'>
+            <img id='icon' src="https://www.ekko-formations.com/assets/logo-6b0f160d3fbbb77ff69d603d9ce0126e80d828e2ff0a37af0e041dc92172be32.png" alt='ekko-logo' />
+          </a>
+          <h1>Test technique réalisé par Aouis CHOUKRI</h1>
+        </div>
+
+        {/* Socials */}
+        <div className="socials">
+          <ul>
+            <li><a href="https://github.com/Aouis20"><img className='logo github' src="https://img.icons8.com/ios-glyphs/344/github.png" alt='aouis-github'/></a></li>
+            <li><a href="https://www.linkedin.com/in/aouis-choukri/"><img className='logo linkedin' src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt='aouis-linkedin'/></a></li>
+            <li><a href="http://aouis.alwaysdata.net"><img className='logo projects' src="https://cdn-icons-png.flaticon.com/512/5956/5956592.png" alt='aouis-projects'/></a></li>
+          </ul>
+        </div>
+
       </header>
-      <section>
-        <h3>Entrer un nombre à traduire...</h3>
-        <form>
-          <label for="number">Nombre :</label>
-          <input type="number" name="number" min="0" max="9" value={number} onChange={e => setNumber(e.target.value)} />
-        </form>
-        <FormTranslate number={number} />
-      </section>
+      
+      {/* Container */}
+      <div className='container'>
+
+        {/* Number Input */}
+        <section>
+          <h2>Entre un nombre et découvre comment l'écrire en anglais  (0-99)</h2>
+          <form>
+            <input type="number" name="number" min="0" max="99" value={number} onChange={e => setNumber(e.target.value)} />
+          </form>
+        </section>
+
+        {/* Translate */}
+        <section>
+          <TranslateSentence number={number} />
+        </section>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/TranslateSentence.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+const TranslateSentence = ({ number }) => {
+  const [enWord, setEnWord] = useState("")
+  const unites = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const tens = ['', 'ten','twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const uniqueNumbers = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  useEffect(() => {
+    if (number.length > 1) {        // 10+
+      if (parseInt(number) !== 10) {      // 10
+        if (parseInt(number)%10 !== 0) {            // 20,30,40...
+          if (parseInt(number) < 20 ) {       // 11,12,12...
+            return setEnWord(uniqueNumbers[number[1]])
+          }
+          return setEnWord(tens[number[number.length-2]] + '-' + unites[number[number.length-1]])
+        }
+        return setEnWord(tens[number[number.length-2]])
+      }
+      return setEnWord(tens[1])
+    } 
+    return setEnWord(unites[number])
+  }, [number])
+
+  return (
+    <div>
+      {enWord && <p>Le {number.length > 1 ? 'nombre' : 'chiffre'} {number} s'écrit <i>"{enWord}"</i> en anglais.</p>}
+    </div>
+  )
+}
+
+export default TranslateSentence
